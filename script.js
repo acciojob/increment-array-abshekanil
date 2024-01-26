9e2b755203cfd58b0d72a279da2d2181758e7619
Abhishek Anil <141896050+abshekanil@users.noreply.github.com>
2024-01-26 17:52:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 let arr = [1, 2, 3, 4, 5];
 function incrementArray(arr) {
   //your code here
+	arr.forEach(function(element,index,array){
+		array[index] = element + 1;
+	});
+	return arr;
 }
-
+ 
 alert(incrementArray(arr));
